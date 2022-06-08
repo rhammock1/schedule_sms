7203a4f8b5192facff0c2eeac21b201bab49ff0d
Robert Hammock <rhamm1027@gmail.com>
2022-06-08 09:47:47 -0400

    init commit

---
+++ schedule_sms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#  Goal:
+#   Have a main plist to run every night and check for files in a folder
+#   If there are files, attempt to schedule the message based on the contents
+#   of the file 
+
+#  Usage: 
+#   Load Launchclt plist to check a folder for files and 
+#   create temp plist for each file to run at specified time.
+#   Delete the file when done, but only after successful send
+#
+#  Content:
+#   message.txt
+#
+#   Contact: Number or Contact Name
+#   Date: 06-10-2022 (MM-DD-YYYY)
+#   Time: 13:00 (24 hour time)
+#   Message: "Words" between quotes for easy tracking
